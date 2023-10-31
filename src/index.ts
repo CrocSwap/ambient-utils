8cf02a3e79685565eff8943ce72024b301d699d0
surudhb <bsurudh@gmail.com>
2023-10-31 16:43:54 -0600

    add addData from ENSAddressContext

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('dev only output');
-  }
-  return a + b;
-};
+// Addresses, ENS addresses 
+export * from './dataLayer/addressManagement/batchFetchAndCacheENSAddressData';
